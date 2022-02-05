5d767ddd5c2ccc687230cf710440c0f3ebc08c83
Michael Burke <michael@mdbooktech.com>
2022-02-05 11:28:16 -0500

    Added script

---
+++ scripts/disable-users.py
@@ -0,0 +1,26 @@
+import os
+import sys
+if sys.version_info[0] < 3:
+    startuser = raw_input("Username to start with: ")
+    whitelist = raw_input("Whitelist (separated by commas): ")
+else:
+    startuser = input("Username to start with: ")
+    whitelist = input("Whitelist (separated by commas): ")
+whitelist = whitelist.split(",")
+
+go = False
+userlist = open("/etc/passwd", "r")
+line = userlist.readline()
+while line:
+    username = line.split(":")[0]
+    if username == startuser:
+        go = True
+    if go and username not in whitelist:
+        os.system("sudo usermod -L " + str(username))
+        os.system("sudo passwd -l " + str(username))
+        os.system("sudo chage -E0 " + str(username))
+        os.system("sudo usermod -s /sbin/nologin " + str(username))
+        print("Disabled " + username)
+    else:
+        print("Skipping " + username)
+    line = userlist.readline()
---
+++ scripts/scp.sh
@@ -0,0 +1,11 @@
+scp scp.sh athena@10.$1.1.40:/home/athena/
+scp scp.sh poseidon@10.$1.2.3:/home/poseidon/
+scp scp.sh demeter@10.$1.2.12:/home/demeter/
+
+scp iptables.sh athena@10.$1.1.40:/home/athena/
+scp iptables.sh poseidon@10.$1.2.3:/home/poseidon/
+scp iptables.sh demeter@10.$1.2.12:/home/demeter/
+
+scp disable-users.py athena@10.$1.1.40:/home/athena/
+scp disable-users.py poseidon@10.$1.2.3:/home/poseidon/
+scp disable-users.py demeter@10.$1.2.12:/home/demeter/
