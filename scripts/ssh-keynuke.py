3813b76969ec65ac56dcf89a7aec03b816cd90f5
MdbookTech <michael@mdbooktech.com>
2022-02-17 13:30:59 -0500

    added repeat option

--- scripts/ssh-keynuke.py
+++ scripts/ssh-keynuke.py
@@ -1,9 +1,28 @@
 import os
 import time
+import sys
 
-os.chdir("/home")
-while True:
+isRepeat = False
+
+def repeat():
+    time.sleep(5)
+    do()
+    repeat()
+
+def do():
     for file in os.listdir("."):
         if os.path.isdir(file):
-            os.system("rm -rf " + file + "/.ssh/authorized_keys")
-    time.sleep(5)
+            pass
+            # os.system("rm -rf " + file + "/.ssh/authorized_keys")
+    
+def main():
+    os.chdir("/home")
+    if len(sys.argv) >= 2:
+        for arg in sys.argv:
+            if arg == "--repeat":
+                isRepeat = True
+    if isRepeat:
+        do()
+        repeat()
+    else:
+        do()
