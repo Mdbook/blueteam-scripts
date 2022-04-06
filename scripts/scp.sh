bee1a234506e43c010814d5f1ebe593ad12916da
MdbookTech <max@mdbook.me>
2022-04-05 18:56:48 -0700

    Removed some sh files

--- scripts/scp.sh
+++
@@ -1,1 +0,0 @@
-scp -r ../../blueteam-scripts user@10.$1.1.2:/home/mdbook
--- scripts/test.sh
+++
@@ -1,1 +0,0 @@
-python3 create-service.py --name=iptables --desc=iptables_service --path=/usr/sbin/iptables-service --command="-l -q $@"
